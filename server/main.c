d44fb1d6c5e120c02db3987b43063f282f8d8d44
tjstone19 <tjstone19@gmail.com>
2015-04-21 10:41:11 -0700

    Initial Commit

---
+++ server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  server
+//
+//  Created by T.J. Stone on 4/21/15.
+//  Copyright (c) 2015 T.J. Stone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
