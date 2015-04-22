02cfb1ffa48b65a0bc17440e1a65cb7224715d59
tjstone19 <tjstone19@gmail.com>
2015-04-22 00:06:58 -0700

    Added Smiths Files

---
+++ server/server.h
@@ -0,0 +1,14 @@
+//
+//  server.h
+//  server
+//
+//  Created by T.J. Stone on 4/21/15.
+//  Copyright (c) 2015 T.J. Stone. All rights reserved.
+//
+
+#ifndef __server__server__
+#define __server__server__
+
+#include <stdio.h>
+
+#endif /* defined(__server__server__) */
